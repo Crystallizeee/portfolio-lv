9f7a36a852386075defb1690757db6bc581a422e
Crystallizeee <89751621+Crystallizeee@users.noreply.github.com>
2026-01-31 00:46:31 +0700

    update minor

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
+        $middleware->trustProxies(at: [
+            // List of trusted proxies (e.g., Cloudflare IP ranges) or '*' for all
+            '*', 
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
